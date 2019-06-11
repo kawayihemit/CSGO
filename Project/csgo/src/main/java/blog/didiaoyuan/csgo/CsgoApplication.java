9fab55c8d1c304d2b1c0e8bca02bdc3357a91f7c
didiaoyuan <didiaoyuan@gmail.com>
2019-06-12 01:03:41 +0800

    project start

---
+++ Project/csgo/src/main/java/blog/didiaoyuan/csgo/CsgoApplication.java
@@ -0,0 +1,13 @@
+package blog.didiaoyuan.csgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsgoApplication.class, args);
+    }
+
+}
---
+++ Project/csgo/src/test/java/blog/didiaoyuan/csgo/CsgoApplicationTests.java
@@ -0,0 +1,16 @@
+package blog.didiaoyuan.csgo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsgoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
